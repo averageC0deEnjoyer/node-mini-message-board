b22ee3239bc4ab0b592e5f450f47a88b2bbd4660
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2023-12-07 21:12:44 +0700

    refactor

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New Message Submission' });
+});
+
+router.post('/new', (req, res, next) => {
+  console.log(req.body);
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
